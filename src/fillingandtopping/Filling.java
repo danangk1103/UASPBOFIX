b6e22768fa48658e99a81c6994bf967fc24b60c7
unknown <danangkurniawan110@gmail.com>
2022-12-30 17:21:34 +0700

    Pembagian tugas dari ketua kelompok

---
+++ src/fillingandtopping/Filling.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package fillingandtopping;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface Filling {
+    public double toppingKeju();
+    public double toppingCoklat();
+    public double toppingKrimVanilla();
+    public double toppingSelaiRedBean();
+    public double toppingSosis();
+    public double toppingSmokedBeef();
+    public double toppingBawangBombay();
+}
---
+++ src/projectkelompok16/ProjectKelompok16.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectkelompok16;
+
+import jenisroti.Pizza;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ProjectKelompok16 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        Pizza pz1 = new Pizza();
+        pz1.varian1(10);
+    }
+    
+}
